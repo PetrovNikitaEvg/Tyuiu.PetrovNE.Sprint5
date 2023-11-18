6e3e72806ce814b8a4bad53622c19a3bfd2f28de
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2023-11-19 02:26:27 +0500

    Task2

---
+++ Tyuiu.PetrovNE.Sprint5.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.PetrovNE.Sprint5.Task2.V9.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint5.Task2.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistFile()
+        {
+            string path = @"C:\Users\niki8\source\repos\Tyuiu.PetrovNE.Sprint5\Tyuiu.PetrovNE.Sprint5.Task2.V9\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            Assert.AreEqual(true, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.PetrovNE.Sprint5.Task2.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PetrovNE.Sprint5.Task2.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PetrovNE.Sprint5.Task2.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3cb2ea9-6455-4f31-bcfd-69b4f8b4b4c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
