42b4de32c5b87bc87538edbe9e2a87af69915b1b
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2023-11-22 17:07:07 +0500

    Task4

---
+++ Tyuiu.PetrovNE.Sprint5.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+using Tyuiu.PetrovNE.Sprint5.Task4.V3.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint5.Task4.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V3.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            Assert.AreEqual(true, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.PetrovNE.Sprint5.Task4.V3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PetrovNE.Sprint5.Task4.V3.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint5.Task4.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Петров Н. Е.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Петров Никита Евгеньевич | АСОиУБ-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение по формуле (Полученное значение округлить до трёх    *");
+            Console.WriteLine("* знаков после запятой) и вернуть полученный результат на консоль.        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V3.txt";
+            Console.WriteLine("Данные находятся в файле " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
