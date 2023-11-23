9a77d30781e156127f05057237ad6285f23fdf2b
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2023-11-23 13:31:11 +0500

    Task7

---
+++ Tyuiu.PetrovNE.Sprint5.Task7.V21/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PetrovNE.Sprint5.Task7.V21.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint5.Task7.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Петров Н. Е.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File                                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Петров Никита Евгеньевич | АСОиУБ-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Удалить все знаки препинания из файла                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask7V21.txt";
+            string pathSaveFile = $@"C:\DataSprint5\OutPutDataFileTask7V21.txt";
+
+            Console.WriteLine("Данные находятся в файле " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.LoadDataAndSave(path);
+
+            Console.WriteLine("Находится в файле: " + res);
+            Console.ReadKey();
+        }
+    }
+}
